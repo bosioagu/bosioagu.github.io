e6c7fd0db3dce44114ccd1dc96162ce43ee32b1e
jorge-lorenzo <jorge.lorenzo@esolutions.com.ar>
2022-12-28 16:32:34 -0300

    postMessage()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,25 @@ export class AppComponent {
   title = 'bosioagu.github.io';
   html = '<h1>Aloha</h1>'
 
+  constructor(){
+    
+  }
+  
   print(){
     console.log("print");
     window.print()
+    
   }
 
   saveLocalStorage(){
     localStorage.setItem("html", this.html)
     console.log(this.html)
+  }
+
+  message() {
+		// parent.postMessage("message to be sent", "http://the-website-that-will-receive-the-msg.com")
+		//parent.postMessage("window:keydown.enter", "*")
+    let message = "envió correcto"
+    window.parent.postMessage(message, '*')
   }
 }
