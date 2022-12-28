e281458c442346232dccc48560dc07b12dc93600
jorge-lorenzo <jorge.lorenzo@esolutions.com.ar>
2022-12-28 11:15:06 -0300

    html localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bosioagu.github.io';
+  html = '<h1>Aloha</h1>'
 
   print(){
     console.log("print");
     window.print()
+  }
+
+  saveLocalStorage(){
+    localStorage.setItem("html", this.html)
+    console.log(this.html)
   }
 }
