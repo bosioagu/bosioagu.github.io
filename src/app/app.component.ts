ca25b43142851ea4b9a6d2789e2b1830576d271f
jorge-lorenzo <jorge.lorenzo@esolutions.com.ar>
2022-12-29 09:30:42 -0300

    html message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,9 +25,11 @@ export class AppComponent {
   }
 
   message() {
+    
 		// parent.postMessage("message to be sent", "http://the-website-that-will-receive-the-msg.com")
 		//parent.postMessage("window:keydown.enter", "*")
-    let message = "envió correcto"
+    let message = '<div class="contenedor-totem"><img class="img-totem" src="./assets/image/totem-body.png"><div class="contenedor-ticket"><div class="img-ticket d-flex flex-column align-items-center justify-content-between"><div class="mt-2 w-100 d-flex flex-column align-items-center"><div class="mt-1 linea-separadora"></div><div class="mt-1 linea-separadora"></div></div><img class="mt-1 ticket-logo" src="./assets/image/logo-2-azul.png"><span class="ticket-numero mt-2"> {{numeroDelTurno}} </span><div class="mb-2 w-100 d-flex flex-column align-items-center"><div class="linea-separadora"></div><span class="ticket-texto-footer"></span> Gracias </span><div class="linea-separadora"></div></div></div></div></div>'
     window.parent.postMessage(message, '*')
+    console.log("mensaje", message)
   }
 }
